7f830b631cfb43b15388f7b73cb0b7432642dfb6
B3 <b3sigma@gmail.com>
2014-05-12 20:40:47 -0700

    Worthless changes to render refactoring.

--- fdRender.h
+++ fdRender.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <tuple>
 #include <stdlib.h>
 #include <stdio.h>
 #include <GL/gl.h>
@@ -9,16 +10,31 @@
 #include <Cg/cgGL.h>
 #include <GL/glut.h>
 
+#include "fdMath.h"
 #include "fdCamera.h"
 
 // Render should contain all the GL code
 // View will contain a specific render target and a camera
 
 class Input {
-  // TODO: actually use any of this.
+  // Wow this is so useful. Good thing it's here.
 };
 
+/*
+  The view is abstracted from the camera as it may be useful
+  to render the same camera and scene onto multiple render targets.
+  Many of the fields could plausibly go in either structure.
+  */
 class View {
+  class Viewport {
+    int _left;
+    int _right;
+    int _top;
+    int _bottom;
+  };
+  float _near;
+  float _far;
+  float _fov;
   Camera _camera;
 };
 
