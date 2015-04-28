f4427fe0a67f1c4af7a8d5dc728b6dbb0c4422a6
b3sigma <b3sigma@gmail.com>
2015-04-28 09:21:47 -0700

    Pass through alpha. Mostly just to clear mod files.

--- data/fragAlphaTestTex.glsl
+++ data/fragAlphaTestTex.glsl
@@ -12,8 +12,8 @@ out vec4 finalColor;
 void main() {
 
   //finalColor = vec4(1.0f, 0.0f, 0.0f, 0.2f);
-  //finalColor = fragCol0;
-  finalColor = texture2D(texDiffuse0, fragTex0);
+  finalColor = fragCol0 * 0.1;
+  finalColor += texture2D(texDiffuse0, fragTex0);
   //finalColor.r = 0.5 + (fragCol0.r * 0.1);
-  finalColor.a = 1.0; //fragCol0.a;
+  finalColor.a = fragCol0.a;
 }
